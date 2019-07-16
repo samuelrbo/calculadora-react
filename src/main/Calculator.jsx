ce23ce4aad9a15611aa88ddd47bd973ccde4bc99
Samuel Ramon <samuelrbo@gmail.com>
2019-07-16 04:11:14 -0300

    Estilizando calculadora

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,28 +5,41 @@ import Button from '../components/Button';
 import Display from '../components/Display';
 
 export default class Calculator extends Component {
+
+  clearMemory = () => {
+    console.log('Limpar');
+  }
+
+  setOperation = (operation) => {
+    console.log(operation);
+  }
+
+  addDigit = (n) => {
+    console.log(n);
+  }
+
   render() {
     return (
       <div className='calculator'>
         <Display valor={100} />
 
-        <Button label='AC' />
-        <Button label='/' />
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='*' />
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
-        <Button label='0' />
-        <Button label='.' />
-        <Button label='=' />
+        <Button label='AC' click={this.clearMemory} triple />
+        <Button label='/' click={this.setOperation} operation />
+        <Button label='7' click={this.addDigit} />
+        <Button label='8' click={this.addDigit} />
+        <Button label='9' click={this.addDigit} />
+        <Button label='*' click={this.setOperation} operation />
+        <Button label='4' click={this.addDigit} />
+        <Button label='5' click={this.addDigit} />
+        <Button label='6' click={this.addDigit} />
+        <Button label='-' click={this.setOperation} operation />
+        <Button label='1' click={this.addDigit} />
+        <Button label='2' click={this.addDigit} />
+        <Button label='3' click={this.addDigit} />
+        <Button label='+' click={this.setOperation} operation />
+        <Button label='0' click={this.addDigit} double />
+        <Button label='.' click={this.addDigit} />
+        <Button label='=' click={this.setOperation} operation />
       </div>
     );
   }
